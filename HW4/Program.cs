80163a96853b23d49a52e2aeef187107224792f2
FoxNik11 <foxnik@inbox.ru>
2024-04-01 01:43:06 +0700

    Добавьте файлы проекта.

---
+++ HW4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace HW4
+{
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+            var s = new Stack();
+            s.Add("a");
+            s.Add("b");
+            s.Add("c");
+            s.Add("d");
+            s.Add("e");
+            s.Add("f");
+            s.Pop();
+            s.Pop();
+            s.Pop();
+            s.Add("f1");
+        }
+    }
+}
---
+++ HW4/Stack.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4
+{
+    public class Stack
+    {
+        private List<string> Elemets = new List<string>();
+        //public Stack(params string[] elString)
+        //{
+        //}
+        //public Stack(string n) => Elemets.Add(n);
+
+        //public Stack(string Elemets)
+        //{
+        //    this.Elemets.Add();
+        //}
+
+        public void Add(string[] ElString)
+        {
+            foreach (var n in ElString)
+            {
+                Elemets.Add(n);
+                Console.WriteLine(n);
+            }
+        }
+        public void Add(string ElString)
+        {
+            Elemets.Add(ElString);
+            size++;
+            top = ElString;
+            Console.WriteLine($"В стек добавлено значение: {ElString}, элементов в стеке: {size}, последний элемент стека: {top}");
+        }
+        public void Pop()
+        {
+            string LastElPop = Elemets.Last();
+            Elemets.RemoveAt(size);
+            size--;
+            top = Elemets.Last();
+            Console.WriteLine($"Из стека удалено значение: {LastElPop}, элементов в стеке: {size}, последний элемент стека: {top}");
+        }
+        private int size=-1;
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+        }
+        private string top;
+        public string Top
+        {
+            get
+            {
+                return top;
+            }
+        }
+    }
+}
